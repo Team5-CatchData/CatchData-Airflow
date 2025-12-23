6c940f429b315117eb84463da81fbffe55e7aae9
JM KIM <kjm9102@gmail.com>
2025-12-23 14:13:56 +0900

    ..

--- catchdata-airflow/dbt/catchdata_dbt/models/staging/stg_kakao_eating_house.sql
+++ catchdata-airflow/dbt/catchdata_dbt/models/staging/stg_kakao_eating_house.sql
@@ -1,8 +1,13 @@
 {{ config(materialized='table') }}
 
+LOAD httpfs;
+
+SET s3_region='ap-northeast-2';
+SET s3_use_ssl=true;
+
 select
   *,
-  regexp_extract(filename, '_(\\d{8})\\.csv', 1) as batch_date
+  regexp_extract(filename, '_([0-9]{8})\.csv', 1) as batch_date
 from read_csv_auto(
   's3://team5-batch/raw_data/kakao/*.csv',
   header=true,
