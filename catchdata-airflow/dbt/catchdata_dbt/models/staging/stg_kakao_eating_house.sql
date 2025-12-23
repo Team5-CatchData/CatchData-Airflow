93d8fc6e18a9b7917fefd245f1c9bf93d64bf2b4
Ubuntu <ubuntu@ip-10-0-136-70.ap-northeast-2.compute.internal>
2025-12-23 04:04:06 +0000

    add dbt project and kakao staging model

--- catchdata-airflow/dbt/catchdata_dbt/models/staging/stg_kakao_eating_house.sql
+++ catchdata-airflow/dbt/catchdata_dbt/models/staging/stg_kakao_eating_house.sql
@@ -1,3 +1,10 @@
 {{ config(materialized='table') }}
 
-select 1 as test;
+select
+  *,
+  regexp_extract(filename, '_(\\d{8})\\.csv', 1) as batch_date
+from read_csv_auto(
+  's3://team5-batch/raw_data/kakao/*.csv',
+  header=true,
+  filename=true
+)
